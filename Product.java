399d5a312d1ef83f11c529de321e2325d1f2db86
urmalhotra <69810354+urmalhotra@users.noreply.github.com>
2020-09-26 03:49:56 +0530

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,123 +1,102 @@
+// --== CS400 File Header Information ==--
+// Name: Uday Malhotra
+// Email: umalhotra@wisc.edu
+// Team: LB
+// Role: Data Wrangler
+// TA: Divyanshu Saxena
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
 import java.util.Random;
+public class Product{
+    private static String type;
+    private static String name;
+    private static String manufacturer;
+    private static Long barcode;
+    private static Double price;
+    private static Random rand;
+    private static String[] names = { "Iphone X", "Ipad Pro", "Macbook Pro", "XPS 13", "Noise Cancelling Series 2",
+            "Inspiron", "Macbook Air", "Chips", "Milk", "Yoghurt", "Cheese", "Chicken Breast", "Frozen Pizza", "Bagels",
+            "Bread", "Cookies", "Sparkling Water", "Energy Drink", "Soda", "Root Beer", "Juice", "Toilet Paper",
+            "Tooth Brush", "Mouthwash", "Toothpaste", "Soap", "Shampoo", "Shower Gel", "Dish Soap", "Towels", "Cutlery",
+            "Sanitizer", "Laundry Detergent", "Fabric Softener", "Dishwasher", "Refrigerator", "TV", "Sound System",
+            "Microwave", "Oven", "Electric Kettle", "Coffee Maker", "Heater", "Humidifier", "Gaming Console", "Pen",
+            "Pencil", "Notepad", "Sticky Notes", "Calendar", "Planner", "Highlighter", "Sharpener", "Notebook",
+            "Journal", "Diary", "Printer", "Trashcan", "Chair", "Tape", "Eraser" };
+    private static String[] types = { "Personal Gadget", "Food", "Home Supplies", "Home Appliances",
+            "Office Supplies" };
+    private static String[] manufacturers = { "Apple", "Dell", "HP", "Lays", "Ruffles", "Doritos", "Chobani", "Lactaid",
+            "Shamrock Farms", "Kraft", "DiGiorno", "Pepperidge Farm", "Klarbrunn", "La Croix", "Perrier", "Red Bull",
+            "Monster", "Pepsi", "Coca Cola", "A & W", "Tropicana", "Kimberley Clark", "P & G", "Colgate", "Crest",
+            "Oral B", "Old Spice", "Dove", "Cetaphil", "Dawn", "Kirkland", "Tide", "Bosch", "LG", "Phillips", "Samsung",
+            "Bose", "Kenmore", "Panasonic", "Keurig", "Black & Decker", "Playstation", "Microsoft", "Staedtler",
+            "Faber Castell", "KOKUYO", "Canson", "Bic", "Durian", "Wipro" };
+    private static Product[] products;
 
-public class Product {
-	private static String type;
-	private static String name;
-	private static double price;
-	private static String manufacturer;
-	private static long barcode;
-	private static Random rand;
-	private static String[] types = { "Personal Gadget", "Food", "Home Supplies", "Home Appliances",
-			"Office Supplies" };
-	private static String[] gadgetNames = { "smartphone", "laptop", "earphones", "smart watch", "chargers",
-			"power bank", "SD Card", "USB drives", "Camera", "Poloroid Camera" };
-	private static String[] foodNames = { "Bread", "Meat", "Rice", "Canned vegetables", "forzen food", "Dairy item",
-			"snacks", "yogurt", "eggs", "Milk", "Oil", "biscuits", "Juices", "Mineral Water", "Ketchup", "Cereal",
-			"Instant food" };
-	private static String[] suppliesNames = { "Tissues", "Toiletry", "Soap", "Handwash", "Shampoo", "ToothPaste",
-			"Lotion", "tootbrush" };
-	private static String[] appliancesNames = { "washing machine", "Television", "Bulbs", "lights", "fans", "Toaster",
-			"stove", "Oven", "Microwave", "Chimney", "Grinder", "air Purifier", "water purifier" };
-	private static String[] officeSuppNames = { "Printer", "Router", "Pens", "Markers", "Papers", "Scanner", "Clock",
-			"Notepad", "paper clips", "calculator", "BUlletin Board" };
-	private static String[][] manufacturerNames = {
-			{ "Apple", "Samsung", "Lenovo", "Dell", "Sony", "HTC", "Windows", "Android", "HP", "IBM", "Intel",
-					"Panasonic", "Hon Nai Precision", "Fuji" },
-			{ "Nestle", "Cadbury", "PepsiCo", "Danone", "Lays", "JBJ", "Frito Lay", "Tyson Foods", "Costco",
-					"Kraft heinz", "Unilever", "Brittania", "Kellogs" },
-			{ "J&J", "Procter & Gamble", "L'Oreal", "Dove", "Tressmee", "Bath and Body Works", "Colgate", "Amyway" },
-			{ "LG", "Whirpool", "ElectroLux", "Haier", "Hitachi", "GoldStar", "Amana Corporation" },
-			{ "Staples", "Office Depot", "3M", "Tesco", "Miniso", "Paper Mate", "Shoplet", "InkStop" } };
-	private static Product[] products;
+    public Product(String name, String type, String manufacturer, Long barcode, Double price) {
+        Product.name = name;
+        Product.type = type;
+        Product.manufacturer = manufacturer;
+        Product.barcode = barcode;
+        Product.price = price;
+    }
 
-	public Product(String name, String type, double price, String manufacturer, long code) {
-		this.name = name;
-		this.type = type;
-		this.price = price;
-		this.manufacturer = manufacturer;
-		this.barcode = code;
-		rand = new Random();
-	}
+    public String getType() {
+        return Product.type;
+    }
 
-	// getter and setter for each private field
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return Product.name;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getManufacturer() {
+        return Product.manufacturer;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public Long getBarcode() {
+        return Product.barcode;
+    }
 
-	public String getType() {
-		return this.type;
-	}
+    public Double getPrice() {
+        return Product.price;
+    }
 
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    public void setType(String type) {
+        Product.type = type;
+    }
 
-	public double getPrice() {
-		return this.price;
-	}
+    public void setName(String name) {
+        Product.name = name;
+    }
 
-	public void setManufacturer(String manu) {
-		this.manufacturer = manu;
-	}
+    public void setManufacturer(String manufacturer) {
+        Product.manufacturer = manufacturer;
+    }
 
-	public String getManufacturer() {
-		return this.manufacturer;
-	}
+    public void setBarcode(Long barcode) {
+        Product.barcode = barcode;
+    }
 
-	public void setBarcode(long code) {
-		this.barcode = code;
-	}
+    public void setPrice(Double price) {
+        Product.price = price;
+    }
 
-	public long getBarcode() {
-		return this.barcode;
-	}
-
-	// need to call put() in this method
-	public static void CreateProducts() {
-		long code;
-		int c;
-		String ty, nm, manu;
-		products = new Product[100];
-		for (int i = 0; i < 100; i++) {
-			code = (long) Math.random() * (9999999999L - 1000000000L + 1) + 1000000000L;
-			c = rand.nextInt(types.length);
-			ty = Product.types[c];
-			switch (c) {
-			case 0:
-				nm = gadgetNames[rand.nextInt(gadgetNames.length)];
-				break;
-			case 1:
-				nm = foodNames[rand.nextInt(foodNames.length)];
-				break;
-			case 2:
-				nm = suppliesNames[rand.nextInt(suppliesNames.length)];
-				break;
-			case 3:
-				nm = appliancesNames[rand.nextInt(appliancesNames.length)];
-				break;
-			case 4:
-				nm = officeSuppNames[rand.nextInt(officeSuppNames.length)];
-				break;
-			case 5:
-				nm = gadgetNames[rand.nextInt(gadgetNames.length)];
-				break;
-			}
-			manu = nm = manufacturerNames[c][rand.nextInt(manufacturerNames[c].length)];
-			products[i] = new Product(nm, ty, Math.random() * 100000 + 1, manu, code);
-
-		}
-	}
-
-	public static void main(String[] args) {
-		CreateProducts();
-	}
-
+    public static void generate() {
+        rand = new Random();
+        int j=0;
+        products = new Product[100];
+        for (int i = 0; i < manufacturers.length; i++){
+            for(int m= 0; m< names.length; m++){
+                for(int n= 0; n< types.length; n++){
+            products[j] = new Product(names[m], types[n],
+                    manufacturers[i], (long) Math.random() * 1000000000000L,
+                    rand.nextFloat()*1000.0);
+                    j++;
+            }  
+        }
+    }  
+    }
+    public static void main(String[] args){
+        generate();
+        System.out.println(manufacturers.length+""+ names.length);
+    }
 }
