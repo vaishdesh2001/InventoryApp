74ee71432b89cae70afe08dc2f65c3c299b14a14
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-09-27 16:49:41 +0530

    This generates listProducts.txt

--- Generation.java
+++ Generation.java
@@ -1,12 +1,6 @@
-// --== CS400 File Header Information ==--
-// Name: Uday Malhotra
-// Email: umalhotra@wisc.edu
-// Team: LB
-// Role: Data Wrangler
-// TA: Divyanshu Saxena
-// Lecturer: Gary Dahl
-// Notes to Grader: <optional extra notes>
 import java.util.Random;
+import java.io.FileWriter;
+
 /**
  * This is the class that initially adds random products to the hash table.
  * 
@@ -14,31 +8,46 @@ import java.util.Random;
  * 
  */
 public class Generation {
-    private static Random rand; 
-    private static Product[] products;
-    // this method generates random products and adds it to the hash table
-    public static void generate() {
-        rand= new Random();
-        products= new Product[3050];
-        int j=0;
-        //looping through all possible manufacturers and product names
-        for (int i = 0; i < Product.manufacturers.length; i++){
-            for(int m= 0; m< Product.names.length; m++){
-                    products[j] = new Product(Product.names[m], Product.types[rand.nextInt(5)],
-                    Product.manufacturers[i], (long) Math.random() * 1000000000000L,
-                    rand.nextFloat()*1000.0);
-                    j++; 
-        }
-    } 
-    
+  private static Random rand;
+  private static Product[] products;
+
+  // this method generates random products and adds it to the hash table
+  public static void generateFile() {
+    rand = new Random();
+    products = new Product[3050];
+    int j = 0;
+    // looping through all possible manufacturers and product names
+    for (int i = 0; i < Product.manufacturers.length; i++) {
+      for (int m = 0; m < Product.names.length; m++) {
+        products[j] =
+            new Product(Product.names[m], Product.types[rand.nextInt(5)], Product.manufacturers[i],
+                (long) Math.random() * 1000000000000L, rand.nextFloat() * 1000.0);
+        j++;
+      }
     }
-    // this method calls the put themod to add these products 
-    // stored in the array
-    public void addProduct(){
-        for(int i=0; i< products.length; i++)
-        InventorySystem.put(products[i].getBarcode(), products[i]);
+    try {
+      FileWriter fw = new FileWriter("src/listProducts.txt");
+      // comma separated file!!!
+      fw.write("name, type, manufacturer, barcode, price\n");
+      for (int i = 0; i < products.length; i++) {
+        fw.write(products[i].getName() + "," + products[i].getType() + ","
+            + products[i].getManufacturer() + "," + products[i].getBarcode() + ","
+            + products[i].getPrice() + "\n");
+      }
+      fw.close();
+    } catch (Exception e) {
+      System.out.println(e);
     }
-    public static void main(String[] args){
-        generate();
-    }
+    System.out.println("wrote!");
+  }
 }
+
+// }
+//
+// // this method calls the put themod to add these products
+// // stored in the array
+// public void addProduct() {
+// for (int i = 0; i < products.length; i++)
+// InventorySystem.put(products[i].getBarcode(), products[i]);
+// }
+
