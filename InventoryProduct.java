32b8de841a925213de02f5c1d82f5928e0216ad3
sneha tripathi <65804001+triha27@users.noreply.github.com>
2020-09-27 15:49:47 +0530

    Update InventoryProduct.java

--- InventoryProduct.java
+++ InventoryProduct.java
@@ -1,6 +1,15 @@
+
+// --== CS400 File Header Information ==--
+// Name: Sneha Tripathi
+// Email: stripathi3@wisc.edu
+// Team: LB
+// TA: Divyanshu Saxena
+// Lecturer: Florian Heimerl
+// Notes to Grader: I was helped by Uday Malhotra (umalhotra@wisc.edu)
 import java.util.Random;
 
 public class InventoryProduct {
+	//fields of the InventoryProduct
 	private static String type;
 	private static String name;
 	private static double price;
@@ -30,6 +39,15 @@ public class InventoryProduct {
 			{ "Staples", "Office Depot", "3M", "Tesco", "Miniso", "Paper Mate", "Shoplet", "InkStop" } };
 	private static InventoryProduct[] products;
 
+	/**
+	 * Parameterized constructor initializing the fields which are passed
+	 * 
+	 * @param name - String name passed by user
+	 * @param type - String the category of product entered by user
+	 * @param price - double price entered by user
+	 * @param manufacturer - String name of the company producing the product
+	 * @param code - long barcode entered
+	 */
 	public InventoryProduct(String name, String type, double price, String manufacturer, long code) {
 		this.name = name;
 		this.type = type;
@@ -40,47 +58,101 @@ public class InventoryProduct {
 	}
 
 	// getter and setter for each private field
+	/**
+	* setter for name privtae field
+	* 
+	* @param name- String name passed to change
+	*/
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	* getter for name privtae field
+	* 
+	* @return name of product
+	*/
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	* setter for setType privtae field
+	* 
+	* @param type  - String type passed to change
+	*/
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	* getter for type privtae field
+	* 
+	* @return type of the product
+	*/
 	public String getType() {
 		return this.type;
 	}
 
-	public void setPrice(float price) {
+	/**
+	* setter for price privtae field
+	* 
+	* @param price- double price passed to change
+	*/
+	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	/**
+	* getter for price privtae field
+	* 
+	* @param return price of the product
+	*/
 	public double getPrice() {
 		return this.price;
 	}
 
+	/**
+	* setter for manufacturer privtae field
+	* 
+	* @param manufacturer - String manufacturer passed to change
+	*/
 	public void setManufacturer(String manu) {
 		this.manufacturer = manu;
 	}
 
+	/**
+	* getter for manufacturer privtae field
+	* 
+	* @return manufacturer of the product
+	*/
 	public String getManufacturer() {
 		return this.manufacturer;
 	}
 
+	/**
+	* setter for barcode privtae field
+	* 
+	* @param code- long code passed to change barcode
+	*/
 	public void setBarcode(long code) {
 		this.barcode = code;
 	}
 
+	/**
+	* getter for barcode privtae field
+	* 
+	* @return barcode of the product
+	*/
 	public long getBarcode() {
 		return this.barcode;
 	}
 
 	// need to call put() in this method
+	/**
+	 * Used to create random products and store them in the hashTable
+	 * 
+	 */
 	public static void CreateProducts() {
 		long code;
 		int c;
@@ -90,6 +162,7 @@ public class InventoryProduct {
 			code = (long) Math.random() * (9999999999L - 1000000000L + 1) + 1000000000L;
 			c = rand.nextInt(types.length);
 			ty = InventoryProduct.types[c];
+			// names and manufacturer are based on the type of product entered by the user
 			switch (c) {
 			case 0:
 				nm = gadgetNames[rand.nextInt(gadgetNames.length)];
@@ -116,6 +189,9 @@ public class InventoryProduct {
 		}
 	}
 
+	/**
+	 * main method used to call CreateProducts()
+	 */
 	public static void main(String[] args) {
 		CreateProducts();
 	}
